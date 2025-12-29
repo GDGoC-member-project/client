beab7c1b137dd8100f6f9ff5aadf2d8b78107d64
Dohyeop Lim <dhlim0817@gmail.com>
2025-12-29 10:08:35 +0900

    feat: add home section placeholders (#1)

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,15 @@
+import AlumniNetwork from "./sections/AlumniNetwork";
 import Billboard from "./sections/Billboard";
+import BuildTogether from "./sections/BuildTogether";
+import OurPeople from "./sections/OurPeople";
 
 export default function Home() {
     return (
-        <>
+        <div className="flex flex-col items-center gap-37.5">
             <Billboard />
-        </>
+            <OurPeople />
+            <AlumniNetwork />
+            <BuildTogether />
+        </div>
     );
 }
---
+++ src/pages/home/sections/BuildTogether/index.tsx
@@ -0,0 +1,10 @@
+export default function BuildTogether() {
+    return (
+        <section>
+            <div className="flex flex-col items-center gap-2">
+                <h2 className="font-title03-medium">Build Together</h2>
+                <p className="font-head02-regular">지금 참여할 수 있는 프로젝트를 만나보세요.</p>
+            </div>
+        </section>
+    );
+}
