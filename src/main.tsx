1029ab5ce8dcefe6a436f3539eed19468cf1ab84
Dohyeop Lim <dhlim0817@gmail.com>
2025-12-29 11:12:49 +0900

    feat: add project uuid routing

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./styles/theme.css";
 import Layout from "./Layout";
 import Home from "./pages/home";
+import ProjectDetail from "./pages/projects/[projectId]";
+import Projects from "./pages/projects";
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
@@ -11,6 +13,11 @@ createRoot(document.getElementById("root")!).render(
             <Routes>
                 <Route element={<Layout />}>
                     <Route index element={<Home />} />
+
+                    <Route path="projects">
+                        <Route index element={<Projects />} />
+                        <Route path=":projectId" element={<ProjectDetail />} />
+                    </Route>
                 </Route>
             </Routes>
         </BrowserRouter>
