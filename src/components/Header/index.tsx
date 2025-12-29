4b878cab7e2521047c06ad8129e9017683330bb2
Dohyeop Lim <dhlim0817@gmail.com>
2025-12-29 09:56:58 +0900

    feat: add header component (#1)

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,7 +2,7 @@ import LogoTyped from "@/components/Logo+Typed";
 
 export default function Header() {
     return (
-        <div className="fixed w-full z-999">
+        <div className="fixed w-full z-999 backdrop-blur-lg">
             <div className="max-w-6xl mx-auto px-4 h-19 py-4 flex items-center justify-between">
                 <LogoTyped />
             </div>
