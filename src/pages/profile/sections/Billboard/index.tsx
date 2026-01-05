8858ca2ac0c57ed7f46bb2cf4634deb809faaff1
Dohyeop Lim <dhlim0817@gmail.com>
2026-01-05 16:12:20 +0900

    fix: correct styling (#17)

--- src/pages/profile/sections/Billboard/index.tsx
+++ src/pages/profile/sections/Billboard/index.tsx
@@ -5,7 +5,7 @@ import SocialLinks from "./components/SocialLinks";
 export default function ProfileBillboard({ profile }: ProfileBillboardProps) {
     return (
         <ContentBillboard>
-            <div className="w-full flex flex-col gap-3">
+            <div className="w-full flex flex-col gap-4">
                 <div className="rounded-full flex items-center justify-center overflow-hidden size-37.5">
                     {profile.profileImage ? (
                         <img
@@ -17,17 +17,17 @@ export default function ProfileBillboard({ profile }: ProfileBillboardProps) {
                         <div className="size-full bg-grey-900" />
                     )}
                 </div>
-                <div className="flex flex-col gap-3">
-                    <h1 className="font-title01-medium">{profile.name}</h1>
-                    <p className="font-body01-medium">
-                        {profile.generation} | {profile.role} | {profile.position}
-                    </p>
-                    {profile.aboutMe && (
-                        <div className="flex items-start justify-between gap-8">
-                            <p className="font-body01-regular text-grey-300">{profile.aboutMe}</p>
-                            <SocialLinks links={profile.links} />
-                        </div>
-                    )}
+                <h1 className="font-title01-medium">{profile.name}</h1>
+                <div className="flex flex-col gap-2">
+                    <div className="flex items-center gap-2 font-body01-medium">
+                        <span>{profile.generation}</span> <div className="w-px h-5 bg-white" />
+                        <span>{profile.role}</span> <div className="w-px h-5 bg-white" />
+                        <span>{profile.position}</span>
+                    </div>
+                    <div className="flex justify-between">
+                        <p className="font-body01-regular">{profile.aboutMe}</p>
+                        <SocialLinks links={profile.links} />
+                    </div>
                 </div>
             </div>
         </ContentBillboard>
