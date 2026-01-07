de8ad810b26e265cbb192919df8f09487dbdaeb1
Dohyeop Lim <dhlim0817@gmail.com>
2026-01-07 13:02:27 +0900

    feat: update content billboard design

--- src/components/ContentBillboard/index.tsx
+++ src/components/ContentBillboard/index.tsx
@@ -1,12 +1,10 @@
-import TilesOverlay from "@/components/TilesOverlay";
-import ContentBillboardBackground from "./Background";
 import type { ContentBillboardProps } from "./types";
+import Mesh from "../Mesh";
 
 export default function ContentBillboard({ children }: ContentBillboardProps) {
     return (
         <section className="relative w-full h-150 overflow-x-hidden">
-            <ContentBillboardBackground />
-            <TilesOverlay />
+            <Mesh />
 
             <div className="relative z-20 h-full flex items-center">
                 <div className="mx-auto w-full max-w-6xl px-4">{children}</div>
