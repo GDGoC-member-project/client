0a9e7c5495bb6755e5bf476f51c592cdfa7474b0
Dohyeop Lim <dhlim0817@gmail.com>
2026-01-07 13:01:28 +0900

    feat: redesign billboard

---
+++ src/components/Mesh/components/Ellipse/index.tsx
@@ -0,0 +1,12 @@
+export default function Ellipse() {
+    return (
+        <div
+            className="border-[0.25px] border-google-blue rounded-full"
+            style={{
+                width: 1116,
+                height: 2122,
+                borderRadius: "50%",
+            }}
+        />
+    );
+}
