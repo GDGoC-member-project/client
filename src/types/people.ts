0198061c8d2fc11a76e2628fa116d4ac00448dfc
Dohyeop Lim <dhlim0817@gmail.com>
2026-01-07 09:57:29 +0900

    feat: update profile type systems (#17)

--- src/types/people.ts
+++ src/types/people.ts
@@ -1,5 +1,11 @@
+/**
+ * @deprecated: Use Role in [profile.ts](./profile.ts)
+ */
 export type MemberRole = "core" | "app" | "frontend" | "backend" | "design" | "ai";
 
+/**
+ * @deprecated: Use ProfileResponse or ProfileSummaryResponse in [profile.ts](./profile.ts)
+ */
 export interface Member {
     id: string;
     name: string;
--- src/types/profile.ts
+++ src/types/profile.ts
@@ -1,5 +1,63 @@
+import type { UUID } from "./common";
 import type { IconType } from "@/components/SocialIcon";
 
+type ProfileBase = {
+    name: string;
+    generation?: number;
+    part?: Part;
+    role?: Role;
+    department?: string;
+    bio?: string;
+    profile_image_url?: string;
+};
+
+type ProfileDetail = {
+    mbti_info?: string;
+    tech_stacks?: string[];
+    social_links?: SocialLink[];
+};
+
+type ProfileIdentity = {
+    user_id: UUID;
+};
+
+export type ProfileRequest = ProfileBase & ProfileDetail;
+
+export type ProfileSummaryResponse = ProfileIdentity & ProfileBase;
+
+export type ProfileResponse = ProfileIdentity & ProfileBase & ProfileDetail;
+
+export enum Part {
+    APP = "APP",
+    FRONTEND = "FRONTEND",
+    BACKEND = "BACKEND",
+    AI = "AI",
+    DESIGN = "DESIGN",
+}
+
+export enum Role {
+    LEAD = "LEAD",
+    CORE = "CORE",
+    MEMBER = "MEMBER",
+}
+
+export enum SocialIcon {
+    GITHUB = "GITHUB",
+    FIGMA = "FIGMA",
+    DRIBBBLE = "DRIBBBLE",
+    INSTAGRAM = "INSTAGRAM",
+    MAIL = "MAIL",
+    LINK = "LINK",
+}
+
+export type SocialLink = {
+    icon?: SocialIcon;
+    url: string;
+};
+
+/**
+ * @deprecated: Use SocialLink and SocialIcon
+ */
 export interface ProfileLink {
     id: string;
     type: IconType;
@@ -13,6 +71,9 @@ export interface ProfileCard {
     url?: string;
 }
 
+/**
+ * @deprecated: Use ProfileResponse
+ */
 export interface Profile {
     id: string;
     name: string;
